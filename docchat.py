1f9e9843459c9a5f5f402584abe4c1420d2ff6d4
Nithya <nithyakrish@gmail.com>
2023-12-06 10:51:11 +0800

    Update docchat.py

--- docchat.py
+++ docchat.py
@@ -21,7 +21,7 @@ def load_data():
     with st.spinner(text="Loading and indexing the PDF docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4", temperature=0.5, system_prompt="Provide the document name only. You are an expert on the MAS documents and your job is to answer technical questions. Assume that all questions are related to the MAS documents. Provide the document name only. Keep your answers technical and based on facts – do not hallucinate features.Always quote the file name from which you retrieved the answer."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4", temperature=0.5, system_prompt="Provide the file name only. You are an expert on the MAS documents and your job is to answer technical questions. Assume that all questions are related to the MAS documents. Provide the file name only. Keep your answers technical and based on facts – do not hallucinate features.Always quote the file name from which you retrieved the answer."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
